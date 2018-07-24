a787d7d5aec1b8aece9ed2d9e06f1ed6421460ec
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-24 21:09:04 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,18 +1,22 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-   sqlite3 students.
+   sqlite3
    "SELECT MAX(GPA) FROM students;"
 
 end
 
 def lowest_student_gpa
+  sqlite3
+  "SELECT MIN(GPA) FROM students;"
 end
 
 def average_student_gpa
 end
 
 def total_tardies_for_all_students
+
+
 end
 
 def average_gpa_for_9th_grade
